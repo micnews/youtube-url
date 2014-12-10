8a2a80db65b64cf69206ddae10abe30c4f7206cd
ralphtheninja <ralphtheninja@riseup.net>
2014-12-10 12:08:01 +0100

    add some more tests

--- test.js
+++ test.js
@@ -2,7 +2,12 @@ var test = require('tape');
 var youtubeUrl = require('./index');
 
 test('youtube url valid test', function (t) {
-  t.plan(2);
   t.equal(youtubeUrl.valid('https://www.youtube.com/watch?v=YoB8t0B4jx4'), true);
+  t.equal(youtubeUrl.valid('https://youtube.com/watch?v=YoB8t0B4jx4'), true);
+  t.equal(youtubeUrl.valid('http://www.youtube.com/watch?v=YoB8t0B4jx4'), true);
+  t.equal(youtubeUrl.valid('http://youtube.com/watch?v=YoB8t0B4jx4'), true);
+  t.equal(youtubeUrl.valid('www.youtube.com/watch?v=YoB8t0B4jx4'), true);
+  t.equal(youtubeUrl.valid('youtube.com/watch?v=YoB8t0B4jx4'), true);
   t.equal(youtubeUrl.valid('https://www.other-url.com/watch?v=YoB8t0B4jx4'), false);
+  t.end()
 });
