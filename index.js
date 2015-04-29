8dcca90d64c2ce61762b104b5209229f5e62dd4f
Dan Flettre <fletd01@yahoo.com>
2015-04-29 01:20:38 -0500

    Match youtu.be, add tests, add extractId

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var urlRegex = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/;
+var urlRegex = /^(?:https?:\/\/)?(?:www\.)?(youtube\.com\/(v\/|embed\/|watch\?.*v=)|youtu\.be\/)((\w|-){11})(?:\S+)?$/;
 
 module.exports = {
   valid: function(url) {
     return !!String(url).match(urlRegex);
+  },
+  extractId: function(url) {
+    var match = urlRegex.exec(url);
+    return match ? match[3] : false;
   }
 };
